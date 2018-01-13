847fc0aabe7996c62e16f9469454197d63a06c59
lpfgarcia <lpfgarcia@gmail.com>
2018-01-13 12:53:08 +0100

    add experiments

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import sys, os
 
 import tensor
-import tensor.log as log
+import tensor.tracelog as log
 import tensor.tools as tools
 from tensor.read import *
 
@@ -17,7 +17,55 @@ if __name__ == "__main__":
   #param = Parameters("TransE_L1", embedding_size=100, max_iter=1000, valid_scores=100)
   #param = Parameters("TransE_L2", embedding_size=100, max_iter=1000, valid_scores=100)
 
-  param = Parameters("Complex_Logistic", embedding_size=5, max_iter=20, valid_scores=10)
+  param = Parameters("Complex_Logistic", embedding_size=100, max_iter=1000, valid_scores=500)
+  model = Experiment(data, train, valid, test, entities, relations, param)
+
+  log.logger.info( "Technique: " + str(param.model))
+  log.logger.info( "Learning rate: " + str(param.learning_rate))
+  log.logger.info( "Max iter: " + str(param.max_iter))
+  log.logger.info( "Generated negatives ratio: " + str(param.neg_ratio))
+  log.logger.info( "Batch size: " + str(param.batch_size))
+
+  model.induce()
+  model.evaluate()
+
+  param = Parameters("DistMult_Logistic", embedding_size=100, max_iter=1000, valid_scores=500)
+  model = Experiment(data, train, valid, test, entities, relations, param)
+
+  log.logger.info( "Technique: " + str(param.model))
+  log.logger.info( "Learning rate: " + str(param.learning_rate))
+  log.logger.info( "Max iter: " + str(param.max_iter))
+  log.logger.info( "Generated negatives ratio: " + str(param.neg_ratio))
+  log.logger.info( "Batch size: " + str(param.batch_size))
+
+  model.induce()
+  model.evaluate()
+
+  param = Parameters("Polyadic_Logistic", embedding_size=100, max_iter=1000, valid_scores=500)
+  model = Experiment(data, train, valid, test, entities, relations, param)
+
+  log.logger.info( "Technique: " + str(param.model))
+  log.logger.info( "Learning rate: " + str(param.learning_rate))
+  log.logger.info( "Max iter: " + str(param.max_iter))
+  log.logger.info( "Generated negatives ratio: " + str(param.neg_ratio))
+  log.logger.info( "Batch size: " + str(param.batch_size))
+
+  model.induce()
+  model.evaluate()
+
+  param = Parameters("TransE_L1", embedding_size=100, max_iter=1000, valid_scores=500)
+  model = Experiment(data, train, valid, test, entities, relations, param)
+
+  log.logger.info( "Technique: " + str(param.model))
+  log.logger.info( "Learning rate: " + str(param.learning_rate))
+  log.logger.info( "Max iter: " + str(param.max_iter))
+  log.logger.info( "Generated negatives ratio: " + str(param.neg_ratio))
+  log.logger.info( "Batch size: " + str(param.batch_size))
+
+  model.induce()
+  model.evaluate()
+
+  param = Parameters("TransE_L2", embedding_size=100, max_iter=1000, valid_scores=500)
   model = Experiment(data, train, valid, test, entities, relations, param)
 
   log.logger.info( "Technique: " + str(param.model))
