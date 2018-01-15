0593c47a60088daa9ab293af6a1e90b7dd941b94
lpfgarcia <lpfgarcia@gmail.com>
2018-01-15 14:36:29 +0100

    add pbs

---
+++ pbs.sh
@@ -0,0 +1,14 @@
+#PBS -N pygpu-test
+#PBS -l select=1:ngpus=1
+#PBS -l walltime=24:00:00  
+
+module load gcc
+module load python
+module load cuda/tools
+module load cudnn/5.1
+
+export PATH="/mnt/nfs/home/lpfgarcia/conda/bin:$PATH"
+
+cd /mnt/nfs/home/lpfgarcia/kge/
+
+DEVICE=cuda0 python run.py --model $model --data $data --k $k --epoch $epoch  --negative $negative> out.log
