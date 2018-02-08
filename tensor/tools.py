b0f32c2c69e91c4130a0b18352508ad48e48f745
lpfgarcia <lpfgarcia@gmail.com>
2018-02-08 16:58:47 +0100

    rename

--- tensor/tools.py
+++ tensor/tools.py
@@ -4,9 +4,9 @@ import numpy as np
 def randn(*args):
   return np.random.randn(*args).astype('f')
 
-def L2_proj(M):
-  norm_M = np.linalg.norm(M,axis=1)
-  M /=  norm_M[:,None]
+def L2(M):
+  norm_M = np.linalg.norm(M, axis=1)
+  M /=  norm_M[:, None]
   return M
 
 class Triples(object):
@@ -17,14 +17,14 @@ class Triples(object):
 
 class Parameters(object):
 
-  def __init__(self, model, lmbda=0.1, k=100, lr=0.5, epoch=1000, bsize=500, 
-    negative=10, sgd='adagrad'):
+  def __init__(self, model='Complex', lmbda=0.1, k=100, lr=0.5, epoch=1000, 
+    bsize=500, negative=10, sgd='adagrad'):
 
     self.model = model
     self.lmbda = lmbda
     self.k = k
     self.lr = lr
     self.epoch = epoch
-    self.batch_size = bsize
+    self.bsize = bsize
     self.neg_ratio = negative
     self.sgd = sgd
