16f12e8a4b6b09489e8b72c1dfa2350abea57c9e
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2018-05-09 15:57:09 +0200

    fixed utf8 encoding issue

--- rdf-to-csv.py
+++ rdf-to-csv.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
+from __future__ import print_function
 import sys
 import argparse
 from rdflib.graph import Graph
 import csv
 
+def to_utf8(lst):
+    return [unicode(elem).encode('utf-8') for elem in lst]
 
 parser = argparse.ArgumentParser(description='Avoiding False Negative Samples on Link Prediction')
 
@@ -26,5 +29,7 @@ writer = csv.writer(sys.stdout, dialect='excel-tab')
 
 for s, p, o in graph:
     row = [ "" + s, "" + p, "" + o ]
-    writer.writerow(row)
-
+    try:
+        writer.writerow(to_utf8(row))
+    except Exception, e:
+         print('Error with row' + str(row) + ':' + str(e), file=sys.stderr)
