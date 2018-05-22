885d680f83e22e30f47e45cbf494bc2efeb1ce40
lpfgarcia <lpfgarcia@gmail.com>
2018-05-22 16:54:10 +0200

    remove export

--- pbs.sh
+++ pbs.sh
@@ -3,10 +3,12 @@
 
 module load gcc
 module load python
-module load cuda/tools
+module load cuda-toolkit/8.0.44
 module load cudnn/5.1
 
 export PATH="/mnt/nfs/home/lpfgarcia/conda/bin:$PATH"
+export DEVICE=cuda0
 
 cd /mnt/nfs/home/lpfgarcia/KGE/
-DEVICE=cuda0 python run.py evaluation --model $model --data $data --k $k --epoch $epoch --folds $folds > $model.$data.$k.$epoch.$folds.log
+python run.py evaluation --model $model --data $data --k $k --epoch $epoch --folds $folds > $model.$data.$k.$epoch.$folds.log
+
