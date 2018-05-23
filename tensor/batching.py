a44dd53276841f6b3028b93f80d484b77be50501
lpfgarcia <lpfgarcia@gmail.com>
2018-05-23 18:57:33 +0200

    refactoring

--- tensor/batching.py
+++ tensor/batching.py
@@ -1,39 +1,37 @@
 import numpy as np
+from random import randint
 
 class Batch(object):
 
   def __init__(self, data, entities, bsize=100, nsize=10):
-    self.data = data
-    self.bsize = bsize
-    self.entities = entities
-    self.nsize = int(nsize)
+    data = data
+    bsize = bsize
+    entities = entities
+    nsize = int(nsize)
 
-    idx = np.random.randint(0, len(self.data.values), self.bsize)
-    self.data.indexes = self.data.indexes[idx,:]
-    self.data.values = self.data.values[idx]
+    idx = np.random.randint(0, len(data.values), bsize)
+    data.indexes = data.indexes[idx,:]
+    data.values = data.values[idx]
 
   def __call__(self):
 
     indexes = []
     values = []
 
-    for i in range(self.bsize):
-      for j in range(self.nsize):
-        aux = self.data.indexes[i]
+    for i in range(bsize):
+      for j in range(nsize):
+        aux = (data.indexes[i]).tolist()
         if np.random.random_sample() < 0.5:
-          aux[0] = np.random.randint(0, self.entities, 1)[0]
+          aux[0] = randint(0, entities)
         else:
-          aux[2] = np.random.randint(0, self.entities, 1)[0]
+          aux[2] = randint(0, entities)
         indexes.append(aux)
         values.append(-1)
 
     indexes = np.array(indexes).astype(np.int64)
     values = np.array(values).astype(np.float32)
 
-    indexes = np.concatenate((indexes, self.data.indexes))
-    values = np.concatenate((values, self.data.values))
+    indexes = np.concatenate((indexes, data.indexes))
+    values = np.concatenate((values, data.values))
 
-    train = [values, indexes[:,0], indexes[:,1], 
-      indexes[:,2]]
-
-    return train
+    return [values, indexes[:,0], indexes[:,1], indexes[:,2]]
